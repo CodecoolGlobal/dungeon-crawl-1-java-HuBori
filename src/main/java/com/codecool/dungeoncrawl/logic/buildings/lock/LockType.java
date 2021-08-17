23a652a828ae982b00369f1d70e06c3497ba6d6c
HuBori <hunyadyb2000@gmail.com>
2021-08-17 14:22:53 +0200

    feat: LockType

--- src/main/java/com/codecool/dungeoncrawl/logic/buildings/lock/LockType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/buildings/lock/LockType.java
@@ -1,4 +1,19 @@
 package com.codecool.dungeoncrawl.logic.buildings.lock;
 
+import com.codecool.dungeoncrawl.logic.items.utility.KeyType;
+
 public enum LockType {
+    DOOR(KeyType.BRONSE),
+    CHEST(KeyType.SILVER),
+    STAIR(KeyType.GOLD);
+
+    private KeyType key;
+
+    LockType(KeyType key) {
+        this.key = key;
+    }
+
+    public KeyType getKey() {
+        return key;
+    }
 }
