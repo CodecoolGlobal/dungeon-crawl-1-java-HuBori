75330f519074af10c287ab6bcc49116a2c4805dc
HuBori <hunyadyb2000@gmail.com>
2021-08-17 14:15:00 +0200

    feat: Lock

--- src/main/java/com/codecool/dungeoncrawl/logic/buildings/lock/Lock.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/buildings/lock/Lock.java
@@ -1,4 +1,35 @@
 package com.codecool.dungeoncrawl.logic.buildings.lock;
 
 public class Lock {
+    private int level;
+    private String detail;
+    private boolean open;
+    private LockType type;
+
+    public Lock(int level, String detail, LockType type) {
+        this.level = level;
+        this.detail = detail;
+        this.type = type;
+        this.open = false;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public LockType getType() {
+        return type;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
 }
