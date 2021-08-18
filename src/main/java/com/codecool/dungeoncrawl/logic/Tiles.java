c792a72c313b6c8b5437a74a5abbbad6fcf98860
HuBori <hunyadyb2000@gmail.com>
2021-08-18 22:58:18 +0200

    refactor + chore: update UML

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/Tiles.java
@@ -0,0 +1,58 @@
+package com.codecool.dungeoncrawl.logic;
+
+import com.codecool.dungeoncrawl.logic.Drawable;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tiles {
+    public static int TILE_WIDTH = 32;
+
+    private static Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
+    private static Map<String, Tile> tileMap = new HashMap<>();
+
+
+    public static class Tile {
+        public final int x, y, w, h;
+        Tile(int i, int j) {
+            x = i * (TILE_WIDTH + 2);
+            y = j * (TILE_WIDTH + 2);
+            w = TILE_WIDTH;
+            h = TILE_WIDTH;
+        }
+    }
+
+    static {
+        tileMap.put("empty", new Tile(0, 0));
+        tileMap.put("wall", new Tile(10, 17));
+        tileMap.put("floor", new Tile(2, 0));
+        tileMap.put("player", new Tile(27, 0));
+        tileMap.put("key", new Tile(18, 23));
+        tileMap.put("weapon", new Tile(0, 31));
+        tileMap.put("default", new Tile(24, 7));
+        tileMap.put("giddy", new Tile(26, 5));
+        tileMap.put("muzzy", new Tile(30, 6));
+        tileMap.put("marching", new Tile(29, 6));
+
+        tileMap.put("door", new Tile(5, 9));
+        tileMap.put("chest", new Tile(8, 6));
+        tileMap.put("stair-down", new Tile(3, 6));
+        tileMap.put("stair-up", new Tile(2, 6));
+    }
+
+    public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
+        Tile tile = tileMap.get(d.getTileName()); // w  = null
+        System.out.println(tileset);
+        System.out.println(tile.x);
+        System.out.println(tile.y);
+        System.out.println(tile.w);
+        System.out.println(tile.h);
+        System.out.println(x* TILE_WIDTH);
+        System.out.println(y*TILE_WIDTH);
+        System.out.println(TILE_WIDTH);
+        context.drawImage(tileset, tile.x, tile.y, tile.w, tile.h,
+                x * TILE_WIDTH, y * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
+    }
+}
