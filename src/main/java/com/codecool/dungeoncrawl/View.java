3c546ee9b3c8ecbb94442e47909730b734cec283
HuBori <hunyadyb2000@gmail.com>
2021-08-18 16:51:44 +0200

    feat: implement view

--- src/main/java/com/codecool/dungeoncrawl/View.java
+++ src/main/java/com/codecool/dungeoncrawl/View.java
@@ -1,5 +1,6 @@
 package com.codecool.dungeoncrawl;
 
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
 
 import java.util.HashMap;
@@ -8,6 +9,38 @@ public class View {
     private final int width = 30;
     private final int height = 20;
     private HashMap<Integer, GameMap> dungeon;
-    private int level = 1;
-    // blabla
+    private int level;
+    private GameMap scenery = new GameMap(level, width, height, CellType.EMPTY);
+
+    public GameMap getScenery() {
+        return scenery;
+    }
+
+    public View(HashMap<Integer, GameMap> dungeon, int level) {
+        this.dungeon = dungeon;
+        this.level = level;
+        setScenery(level);
+    }
+
+    public void setScenery(int level) {
+        GameMap map = dungeon.get(level);
+        int x = (map.getPlayer().getX() - (width/2) < 0) ? 0 : map.getPlayer().getX() - (width/2);
+        int y = (map.getPlayer().getY() - (height/2)) < 0 ? 0 : map.getPlayer().getY() - (height/2);
+        x = (x < map.getWidth()) ? x : map.getWidth() - 1;
+        y = (y < map.getHeight()) ? y : map.getHeight() - 1;
+        for (int i = 0; i + x < map.getPlayer().getX() + (width/2) && i + x < map.getWidth(); i++) {
+            for (int j = 0; j + y < map.getPlayer().getY() + (height/2) && j + y < map.getHeight(); j++) {
+                scenery.getCell(i, j).setCell(map.getCell(x+i, y+j));
+            }
+        }
+        this.scenery = scenery;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
